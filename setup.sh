7d16254fdef74fe75ee2876e2c3f87856d44d301
Michael Arnold <michaelaarnold@gmail.com>
2019-10-14 16:43:24 -0700

    change default shell to zsh and add comments

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+# get absolute path of script, regardless caller path
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install stuff
 brew cask install lastpass intellij-idea visual-studio-code 
 brew install zsh git antigen nvm awscli haskell-stack rbenv
 
-script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# create empty .secrets file if it hasn't already been made
+touch "$script_path/.secrets"
 
+# symlink dotfiles and plists into home dir
 files=$(ls -a | grep "^\.[a-z]" | grep -v "^\.git")
 files+=" $(ls -a | grep *plist*)"
-
-touch "$script_path/.secrets"
 
 for file in $files
 do
@@ -17,3 +21,6 @@ do
 	echo "symlinking $file_symlink to $file_origin"
 	ln -sf $file_origin $file_symlink
 done
+
+# change default shell to zsh
+chsh -s `which zsh`
