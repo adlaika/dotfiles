f422b87d9a630d35f886196b8bda09a3b18cd8ff
Michael Arnold <michaelaarnold@gmail.com>
2019-10-15 13:46:28 -0700

    only do stuff if not done already, add more installs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-# get absolute path of script, regardless caller path
+# get absolute path of script, regardless of caller path
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# install brew if not already
+brew --version > /dev/null
+if [[ $? -eq 1 ]]; then
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# enable brew cask
+brew tap homebrew/cask-versions
+
 # install stuff
-brew cask install lastpass intellij-idea visual-studio-code 
-brew install zsh git antigen nvm awscli haskell-stack rbenv postgres
+brew cask install java lastpass intellij-idea visual-studio-code iterm2 firefox google-chrome 
+brew install zsh git antigen nvm awscli haskell-stack rbenv postgres scala bash coreutils gti maven node tree wget graphviz openssl python@2 vim watch bash-completion jq
 
 # start services and add to startup
 brew services start postgresql
@@ -16,7 +25,6 @@ touch "$script_path/.secrets"
 # symlink dotfiles and plists into home dir
 files=$(ls -a | grep "^\.[a-z]" | grep -v "^\.git")
 files+=" $(ls -a | grep *plist*)"
-
 for file in $files
 do
 	file_origin="$script_path/$file"
@@ -25,5 +33,11 @@ do
 	ln -sf $file_origin $file_symlink
 done
 
-# change default shell to zsh
-chsh -s `which zsh`
+# change default shell to zsh if not already
+DEFAULT_SHELL=$(dscl . -read ~/ UserShell | sed 's/UserShell: //')
+if [[ "$DEFAULT_SHELL" != *"/zsh" ]]; then
+	ZSH_LOCATION=$(which zsh)
+	grep -qF "$ZSH_LOCATION" /etc/shells  || echo "$ZSH_LOCATION" | sudo tee -a /etc/shells
+	# sudo echo "$(which zsh)" >> /etc/shells
+	chsh -s "$ZSH_LOCATION"
+fi
